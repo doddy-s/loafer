9489dfcde54bb43d5788d9979ec22c068fdbbb0b
doddy-s <doddy.s@students.amikom.ac.id>
2023-08-31 20:05:29 +0700

    backbones

---
+++ src/app/+Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="h-auto w-screen px-20 py-10 flex items-center justify-between fixed">
+        <h1>LoafeR</h1>
+        <h1>Search</h1>
+      </nav>
+    </>
+  )
+}
