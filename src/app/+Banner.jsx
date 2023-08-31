c9a9cd220606b91f8c2daa8e0067cc6aedf20f41
doddy-s <doddy.s@students.amikom.ac.id>
2023-08-31 20:29:13 +0700

    Adds banner

--- src/app/+Banner.jsx
+++ src/app/+Banner.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from 'next/image'
 
 export const Banner = () => {
   return (
     <>
-      <div>Banner</div>
+      <div className='mask-banner'>
+        <Image
+          src="https://cdn.discordapp.com/attachments/1069940642521825320/1146794434168619101/cropped-1920-1080-1309318.jpg"
+          width={1920}
+          height={1080}
+          alt="banner"
+        />
+      </div>
     </>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Banner } from './+Banner'
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			
-		</main>
-	)
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Banner />
+      <div className="h-screen w-screen bg-cyan-200"></div>
+    </main>
+  )
 }
